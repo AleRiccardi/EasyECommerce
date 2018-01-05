75226fc5fb53904b94c59e76370d0134567077d3
AleRiccardi <alessandro.riccardi4@studio.unibo.it>
2018-01-05 19:16:05 +0100

    Created the header and footer funzionality and connected the php engine.

---
+++ curiosita.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * Curiosita page.
+ */
+
+require_once (dirname(__FILE__) . "/template/header.php");
+?>
+
+<!--
+    <section class="orizontal-centering margintop notsticky paddedown" >
+        <div>
+            <img class="vertical-centering orizontal-centering" src="img/sushi-01.jpg" alt="Sushi-dish" height="50%" width="100%">
+        </div>
+        <div class="vertical-centering">
+            <h1 class="ltitle">CHI SIAMO</h1>
+            <p>
+                Siamo un’azienda italiana che parla giapponese.
+                Siamo per la contaminazione culturale, amiamo fondere l’eccellenza di paesi diversi.
+                Siamo per la bontà e anche per la bellezza, perché mangiare è un’esperienza multisensoriale.
+                Siamo protettori delle tradizioni e guerrieri delle innovazioni.
+                Siamo perfezionisti della qualità e artisti del gusto.
+                Siamo ZB Sushi.
+            </p>
+        </div>
+
+
+    </section >
+
+
+    <section class="orizontal-centering margintop paddedown">
+        <div class="vertical-centering">
+            <h2 class="ltitle">IL FUTURO STORICO.</h2>
+            <p>
+                La tradizione del sushi affonda le sue radici nel VII secolo,
+                quando in Giappone si iniziò a preparare il nare-sushi,
+                l’antenato marinato della più famosa pietanza nipponica.
+                In seguito, nei primi anni del 1800, avvenne la grande rivoluzione.
+                Lo chef Hanaya Yohei introdusse l’uso del sashimi nella preparazione del sushi,
+                omplementando le tecniche che ancora oggi vengono utilizzate e preparandolo
+                davanti agli occhi del cliente, così come avviene nei nostri ristoranti.</p>
+        </div>
+        <div>
+            <img  src="http://static.pourfemme.it/pfricette/fotogallery/845X0/24915/mega-piatto-di-sushi.jpg" alt="Sushi-dish" height="250%" width="160%">
+        </div>
+    </section>
+
+
+    <section class="orizontal-centering margintop notsticky paddedown">
+        <div>
+            <img class="vertical-centering orizontal-centering" src="img/sushi-01.jpg" alt="Sushi-dish" height="35%" width="70%">
+        </div>
+        <div class="vertical-centering">
+            <h3 class="ltitle">CIAO</h3>
+            <p class="">Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                sed do eiusmod tempor incididunt ut labore et dolore magna
+                aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                Duis aute irure dolor in reprehenderit in voluptate velit
+                esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+                occaecat cupidatat non proident, sunt in culpa qui officia
+                deserunt mollit anim id est laborum.</p>
+        </div>
+
+
+
+    </section> -->
+
+
+<?php require_once (dirname(__FILE__) . "/template/footer.php");
