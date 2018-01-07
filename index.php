7b5745f857f2347707aabc27009df97eafb62dce
AleRiccardi <alessandro.riccardi4@studio.unibo.it>
2018-01-07 17:25:01 +0100

    Added home template.

--- index.php
+++ index.php
@@ -3,58 +3,12 @@
  * Single page.
  */
 
+//Header
 require_once(dirname(__FILE__) . "/engine.php");
 require_once(dirname(__FILE__) . "/template/_header.php");
 
-?>
+//Content
+require_once(dirname(__FILE__) . "/template/home.php");
 
-    <section class="flex-container-center fit-height-section"
-             style="background: url('<?php echo $baseController->website_url ?>/assets/img/sushi-table.jpg');">
-        <div class="flex-item-center">
-            <div class="logo-container">
-                <img src="<?php echo $baseController->website_url ?>/assets/img/logo.png" alt="logo">
-            </div>
-            <div>
-                <h1 class="home-title ">Experience <br> the taste of Japan</h1>
-            </div>
-        </div>
-    </section>
-
-    <section class="hs-second">
-        <div class="container">
-            <h1 class="title-center">Our qualities</h1>
-            <div class="row">
-                <div class="col-4 flex-container-center">
-                    <div class="flex-item-center">
-                        <p>
-                            Sushi Lounge is the Concept Store where the tradition of Asian cuisine combines with Mediterranean excellence.
-                        </p>
-                    </div>
-                </div>
-                <div class="col-4 flex-container-center">
-                    <div class="flex-item-center">
-                        <img width="100%" src="<?php echo $baseController->website_url ?>/assets/img/sushi/1.png"
-                             alt="Funny sushi">
-                    </div>
-                </div>
-                <div class="col-4 flex-container-center">
-                    <div class="flex-item-center">
-                        <p>
-                            Sushi & Go is the line of recipes designed for Take Away.
-                        </p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-
-    <section class="flex-container-center fit-height-section hs-third"
-             style="background: url('<?php echo $baseController->website_url ?>/assets/img/japan-street.jpg');">
-        <div class="flex-item-center">
-            <p class="lead-home">The best restaurant that connects the Japanese kitchen with a simple Vespa.
-            </p>
-            <span class="cit-right">Rolling Stone</span>
-        </div>
-    </section>
-
-<?php require_once(dirname(__FILE__) . "/template/_footer.php");
+//Footer
+require_once(dirname(__FILE__) . "/template/_footer.php");
