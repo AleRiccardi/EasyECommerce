e51dc82bd344fc4a1307ae6a33ee5be9d51a73d1
AleRiccardi <alericcardi1996@gmail.com>
2018-01-07 17:01:39 +0100

    Database classes able to take information, user page improved.

--- template/_header.php
+++ template/_header.php
@@ -15,45 +15,43 @@ $login = new \Inc\Classes\Login();
 <body>
 <header>
     <nav class="navbar">
-        <div class="container">
-            <div class="cont-logo">
-                <img id="logo" src="<?php echo $baseController->website_url ?>/assets/img/logo.png" alt="logo"
-                     width="70px"
-                     height="70px">
+        <div class="cont-logo">
+            <img id="logo" src="<?php echo $baseController->website_url ?>/assets/img/logo.png" alt="logo"
+                 width="70px"
+                 height="70px">
+        </div>
+
+        <div class="wrap-menus">
+            <div class="cont-menu">
+                <ul class="nav-menu">
+                    <li class="nav-item active"><a class="nav-link" href="./"> Home</a></li>
+                    <li class="nav-item"><a class="nav-link" href="./curiosity.php">Curiosity</a></li>
+                    <?php if (!$login->isUserLoggedIn()) { ?>
+                        <li class="nav-item"><a class="nav-link" href="./login.php">Login</a></li>
+                        <li class="nav-item"><a class="nav-link" href="./registration.php">Registration</a></li>
+                    <?php } ?>
+                </ul>
             </div>
 
-            <div class="wrap-menus">
-                <div class="cont-menu">
-                    <ul class="nav-menu">
-                        <li class="nav-item active"><a class="nav-link" href="./"> Home</a></li>
-                        <li class="nav-item"><a class="nav-link" href="./curiosity.php">Curiosity</a></li>
-                        <?php if (!$login->isUserLoggedIn()) { ?>
-                            <li class="nav-item"><a class="nav-link" href="./login.php">Login</a></li>
-                            <li class="nav-item"><a class="nav-link"href="./registration.php">Registration</a></li>
-                        <?php } ?>
-                    </ul>
-                </div>
-
-                <?php if ($login->isUserLoggedIn()) { ?>
-                    <div class="cont-menu-user">
-                        <div class="nav-item dropdown">
-                            <!--<a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">Dropdown</a>-->
-                            <div class="mu-info " href="http://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-                                <div class="mu-img">
-                                    <div class="profile-image"></div>
-                                </div>
-                                <span class="mu-dd-button"><?php echo $_SESSION['user_name']; ?></span>
+            <?php if ($login->isUserLoggedIn()) { ?>
+                <div class="cont-menu-user">
+                    <div class="nav-item dropdown">
+                        <div class="mu-info" id="dropdown01" data-toggle="dropdown"
+                             aria-haspopup="true" aria-expanded="true">
+                            <div class="mu-img">
+                                <div class="profile-image" style="background-image: url('<?php echo $baseController->website_url ?>/assets/img/icon/user-image-small.png');"></div>
                             </div>
-                            <div class="dropdown-menu mu-dd-content " aria-labelledby="dropdown01">
-                                <a class="dropdown-item" href="<?php echo $baseController->website_url ?>/page.php?name=user">User</a>
-                                <a class="dropdown-item" href="#">Cart</a>
-                                <a class="dropdown-item" href="<?php echo $baseController->website_url ?>/login.php?logout">Logout</a>
-                            </div>
+                            <span class="mu-dd-button"><?php echo $_SESSION['user_name']; ?></span>
+                        </div>
+                        <div class="dropdown-menu mu-dd-content " aria-labelledby="dropdown01">
+                            <a class="dropdown-item"
+                               href="<?php echo $baseController->website_url ?>/page.php?name=user">User</a>
+                            <a class="dropdown-item" href="#">Cart</a>
+                            <a class="dropdown-item" href="<?php echo $baseController->website_url ?>/login.php?logout">Logout</a>
                         </div>
                     </div>
-                <?php } ?>
-            </div>
-        </div>
+                </div>
+            <?php } ?>
         </div>
     </nav>
 </header>
--- template/user.php
+++ template/user.php
@@ -1,10 +1,66 @@
-<section class="flex-container-center home-section">
-    <div class="flex-item-center">
-        <h1><?php  ?></h1>
-        <p>
-            Hey, <?php echo $_SESSION['user_name']; ?>. You are logged in.
-            Try to close this browser tab and open it again. Still logged in!
-        </p>
-        <a href="index.php?logout">Logout</a>
+<?php
+
+use Inc\Database\DbUser;
+
+//$res = DbUser::get(array("user_name" => $_SESSION['user_name']), "ARRAY_A");
+$res = DbUser::get(array("user_name" => $_SESSION['user_name']), "OBJECT");
+?>
+<div class="page-user fit-height-section">
+
+    <div class="jumbotron user-header">
+        <div class="container">
+            <div class="user-img-name middle-h-container">
+                <div class="pu-img ">
+                    <div class="profile-image" style="background-image: url('<?php echo $baseController->website_url ?>/assets/img/icon/user-image-small.png');"></div>
+                </div>
+                <div class="middle-h-item user-name">
+                    <h1 class=""><?php echo $res[0]->user_name; ?></h1>
+                    <span>Registered the <?php echo "20 Jan 17"; ?></span>
+                </div>
+            </div>
+        </div>
     </div>
-</section>
+    <section class="user-body">
+        <div class="container">
+            <!-- Example row of columns -->
+            <div class="row flex-container">
+                <div class="user-action flex-item col-md-3">
+                    <a href="<?php echo $baseController->website_url ?>/page.php?name=user">
+                        <div class="icon-action">
+                            <img src="<?php echo $baseController->website_url ?>/assets/img/icon/cart.png" alt="logo">
+                        </div>
+                        <h3>Cart</h3>
+                        <p>Your current cart list</p>
+                    </a>
+                </div>
+                <div class="user-action flex-item col-md-3">
+                    <a href="<?php echo $baseController->website_url ?>/page.php?name=user">
+                        <div class="icon-action">
+                            <img src="<?php echo $baseController->website_url ?>/assets/img/icon/box.png" alt="logo">
+                        </div>
+                        <h3>Your Order</h3>
+                        <p>List of all your order</p>
+                    </a>
+                </div>
+                <div class="user-action flex-item col-md-3">
+                    <a href="<?php echo $baseController->website_url ?>/page.php?name=user">
+                        <div class="icon-action">
+                            <img src="<?php echo $baseController->website_url ?>/assets/img/icon/truck.png" alt="logo">
+                        </div>
+                        <h3>Your Address</h3>
+                        <p>Edit addresses for orders</p>
+                    </a>
+                </div>
+                <div class="user-action flex-item col-md-3">
+                    <a href="<?php echo $baseController->website_url ?>/login.php?logout">
+                        <div class="icon-action">
+                            <img src="<?php echo $baseController->website_url ?>/assets/img/icon/id-card.png" alt="logo">
+                        </div>
+                        <h3>Login</h3>
+                        <p>Edit login, name, password </p>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
